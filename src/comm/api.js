81001794559d7bc0a04207f7ec52daaccd55851d
zoueature <zoueature@gmail.com>
2019-10-14 00:05:47 +0800

    接入flag的创建获取以及登录接口

---
+++ src/comm/api.js
@@ -0,0 +1,9 @@
+let mileStoneDomain =  'http://192.168.8.202:8080'
+let ucDomain = 'http://127.0.0.1:9090'
+
+module.exports = {
+  wxLogin: ucDomain + '/Login/thirdLogin',
+  flagList: mileStoneDomain + '/flag/list',
+  addFlag: mileStoneDomain + '/flag/add',
+  taskList: mileStoneDomain + '/task/all'
+}
