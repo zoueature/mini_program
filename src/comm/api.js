85215cc1e224e50ae9270f46fbbed1a89806a6aa
zoueature <zoueature@gmail.com>
2019-10-20 22:53:14 +0800

    修改打卡周期的交互

--- src/comm/api.js
+++ src/comm/api.js
@@ -2,9 +2,13 @@ let mileStoneDomain =  'http://192.168.8.202:8080'
 let ucDomain = 'http://127.0.0.1:9090'
 
 module.exports = {
+  //user
   wxLogin: ucDomain + '/Login/thirdLogin',
+  getUserInfo: mileStoneDomain + '/user/info',
+  //flag
   flagList: mileStoneDomain + '/flag/list',
   addFlag: mileStoneDomain + '/flag/add',
+  taskCheckIn: mileStoneDomain + '/flag/checkIn',
+  //task
   taskList: mileStoneDomain + '/task/all',
-  taskCheckIn: mileStoneDomain + '/flag/checkIn'
 }
