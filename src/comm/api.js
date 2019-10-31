c66cbc20ae64ba27a3ceaff297af2b2df864e791
zoueature <zoueature@gmail.com>
2019-10-31 23:10:01 +0800

    每次启动小程序时同步一次分类数据

--- src/comm/api.js
+++ src/comm/api.js
@@ -1,7 +1,7 @@
 let mileStoneDomain =  'http://192.168.8.202:8080'
 let ucDomain = 'http://127.0.0.1:9090'
 
-mileStoneDomain = 'http://127.0.0.1:8080'
+//mileStoneDomain = 'http://127.0.0.1:8080'
 
 module.exports = {
   //user
